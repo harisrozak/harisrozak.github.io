0444ef2faa8bf4ba20835694a34d8082a0938150
Haris Ainur Rozak <harisrozak@gmail.com>
2019-09-26 14:22:28 +0700

    added todos react project

---
+++ react/firebase-todos/precache-manifest.68a921b0d24dd3cc6293e51caebbc7af.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a9ff7ce9eb2e74f56444668f09c2156",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c5dc928592fcd7d390c7",
+    "url": "./static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "16a00c07663b75d4d4cd",
+    "url": "./static/css/main.fb807e44.chunk.css"
+  },
+  {
+    "revision": "c5dc928592fcd7d390c7",
+    "url": "./static/js/2.1cc33781.chunk.js"
+  },
+  {
+    "revision": "16a00c07663b75d4d4cd",
+    "url": "./static/js/main.88767484.chunk.js"
+  },
+  {
+    "revision": "cb435458f8710fb1aa69",
+    "url": "./static/js/runtime-main.5811dcd6.js"
+  }
+]);
