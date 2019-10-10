0cd91f3e5c14c62c7b5d1dcd0b4e5f53947078bc
Haris Ainur Rozak <harisrozak@gmail.com>
2019-10-10 15:55:56 +0700

    added ayah-finder app

---
+++ react/ayah-finder/precache-manifest.542558183a6197e4e0518a500e1e70a3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b360ee7887ddc4f9c5a8d1df589bacf",
+    "url": "./index.html"
+  },
+  {
+    "revision": "286e063932ababfac272",
+    "url": "./static/css/2.510409cd.chunk.css"
+  },
+  {
+    "revision": "8d9a333ffe3541530fbd",
+    "url": "./static/css/main.0d812a9d.chunk.css"
+  },
+  {
+    "revision": "286e063932ababfac272",
+    "url": "./static/js/2.bd421431.chunk.js"
+  },
+  {
+    "revision": "8d9a333ffe3541530fbd",
+    "url": "./static/js/main.a8ffed73.chunk.js"
+  },
+  {
+    "revision": "cb624b1ac78526f0ef5d",
+    "url": "./static/js/runtime-main.d9fca96c.js"
+  }
+]);
